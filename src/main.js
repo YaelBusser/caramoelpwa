cd2dbb545e82b30ae305ab0864b8eba5a63fcbfb
Yaël Busser <mrmaclevine@gmail.com>
2023-05-18 06:57:35 +0200

    ok

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-import { createApp } from './node_modules/vue';
-import './style.css';
-import App from "./App.vue";
-import router from './router/index.js';
-import store from "../store.js";
+import { createApp } from '/node_modules/vue';
+import '/src/style.css';
+import App from "/src/App.vue";
+import router from '/src/router/index.js';
+import store from "/src/store.js";
 
 const app = createApp(App);
 app.use(router).use(store).mount('#app');
