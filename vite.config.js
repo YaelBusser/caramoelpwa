885310b8a7066736653a604287341afbe4b3a4b2
Yaël Busser <mrmaclevine@gmail.com>
2023-05-18 07:11:43 +0200

    ok

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  optimizeDeps: {
-    exclude: ['vue']
-  },
-  rollupOptions: {
-    external: ['vue']
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
   }
 })
